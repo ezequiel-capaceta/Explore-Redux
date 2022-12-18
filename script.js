201d8e4a68fb929e5d939c24e05b272643372b86
ezequiel-capaceta <108444379+ezequiel-capaceta@users.noreply.github.com>
2022-12-18 11:16:36 -0800

    first commit

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 const valueEl = document.getElementById('value')
 const plusBtn = document.getElementById('plus')
 const minusBtn = document.getElementById('minus')
+const plusFiveBtn = document.getElementById('plus-five')
+const minusFiveBtn = document.getElementById('minus-five')
+const ifOddBtn = document.getElementById('increment-odd')
+const asyncBtn = document.getElementById('increment-async')
+const customInput = document.getElementById('custom-amount')
+const customBtn = document.getElementById('submit-custom')
 
 // initial state value
 const initialState = {
@@ -15,8 +21,14 @@ const counterReducer = (state = initialState, action) => {
             return { value: state.value + 1 }
         case 'counter/decremented':
             return { value: state.value - 1 }
+        case 'counter/incrementFive':
+            return { value: state.value + 5 }
+        case 'counter/decrementFive':
+            return { value: state.value - 5 }
+        case 'counter/custom':
+            return { value: state.value + action.payload }
         default:
-        return state
+            return state
     }
 }
 
@@ -27,6 +39,18 @@ const addAction = {
 
 const subAction = {
   type: 'counter/decremented'
+}
+
+const addFiveAction = {
+    type: 'counter/incrementFive'
+}
+
+const subFiveAction = {
+    type: 'counter/decrementFive'
+}
+
+let customAction = {
+    type: 'counter/custom'
 }
 
 // generating the store
@@ -47,9 +71,40 @@ const subOne = () => {
   store.dispatch(subAction)
 }
 
+const addFive = () => {
+    store.dispatch(addFiveAction)
+}
+
+const subFive = () => {
+    store.dispatch(subFiveAction)
+}
+
+const incrementIfOdd = () => {
+    state = store.getState()
+    if (state.value % 2 !== 0) {
+        store.dispatch(addAction)
+    }
+}
+
+const incrementAsync = () => {
+    setTimeout(() => {
+        store.dispatch(addAction)
+    }, 1000);
+}
+
+const changeCustom = () => {
+    customAction = {...customAction, payload: +customInput.value}
+    store.dispatch(customAction)
+}
+
 // event listeners
 plusBtn.addEventListener('click', addOne)
 minusBtn.addEventListener('click', subOne)
+plusFiveBtn.addEventListener('click', addFive)
+minusFiveBtn.addEventListener('click', subFive)
+ifOddBtn.addEventListener('click', incrementIfOdd)
+asyncBtn.addEventListener('click', incrementAsync)
+customBtn.addEventListener('click', changeCustom)
 
 // initial render
 render()
